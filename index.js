df4dfb1c9c0e4517d97016366e9b35c3e04e9d8c
Gpioblink <gpioblink@aol.com>
2019-02-28 11:56:54 +0900

    とりあえずハリボテAPIを返す

---
+++ index.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var app = express();
+
+var server = app.listen(3000, function(){
+    console.log("Node.js is listening to PORT:" + server.address().port);
+});
+
+var subsList = [
+    {
+        startTime: "0.33",
+        endTime: "3.22",
+        text: "Hello! My name is Ai Ueo!!",
+        details: [
+            {
+                startIdx: "0",
+                endIdx: "4",
+                text: "こんにちは"
+            }
+        ]
+    }
+]
+
+app.get("/subpurseapi/test.json", function(req, res, text){
+    res.json(subsList);
+});
